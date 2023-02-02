241ee2d6f017973a67f8c0ef93eb2879edd900b4
adbenoit-9 <adeline.benoit@hotmail.com>
2023-02-02 17:13:25 +0100

    [NORM]

--- Matt_daemon.cpp
+++ Matt_daemon.cpp
@@ -6,17 +6,19 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 18:34:07 by adbenoit          #+#    #+#             */
-/*   Updated: 2023/01/31 12:04:31 by adbenoit         ###   ########.fr       */
+/*   Updated: 2023/02/02 17:12:12 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Matt_daemon.hpp"
 
-Matt_daemon::Matt_daemon(int fd_lock) : _fd_lock(fd_lock) {
+Matt_daemon::Matt_daemon(int fd_lock) : _fd_lock(fd_lock)
+{
 	Tintin_reporter::write_logs(Tintin_reporter::inf, "Started.");
 }
 
-Matt_daemon::~Matt_daemon() {
+Matt_daemon::~Matt_daemon()
+{
 	try {
 		Tintin_reporter::write_logs(Tintin_reporter::inf, "Quitting.");
 	}
@@ -25,10 +27,11 @@ Matt_daemon::~Matt_daemon() {
 		this->unlock_file();
 		close(this->_fd_lock);
 	}
-	remove(LOCK_FILE);	
+	remove(LOCK_FILE);
 }
 
-void    Matt_daemon::init_server(void) {
+void    Matt_daemon::init_server(void)
+{
 	int	ret;
 
 	Tintin_reporter::write_logs(Tintin_reporter::inf, "Creating server.");
@@ -36,13 +39,14 @@ void    Matt_daemon::init_server(void) {
 	if (ret == s_ko) {
 		Tintin_reporter::write_logs(Tintin_reporter::err, "Server failed to create.");
 		throw FunctionError();
-	} 
+	}
 	Tintin_reporter::write_logs(Tintin_reporter::inf, "Server created.");
 }
 
-void    Matt_daemon::lock_file() {
+void    Matt_daemon::lock_file()
+{
 	int	ret;
-	
+
 	ret = flock(this->_fd_lock, LOCK_EX | LOCK_NB);
 	if (ret == -1) {
 		Tintin_reporter::write_logs(Tintin_reporter::err, "Error file locked.");
@@ -50,14 +54,16 @@ void    Matt_daemon::lock_file() {
 	}
 }
 
-void    Matt_daemon::unlock_file() {
+void    Matt_daemon::unlock_file()
+{
 	flock(this->_fd_lock, LOCK_UN);
 }
 
-void	Matt_daemon::_log_clients_input(void) {
+void	Matt_daemon::_log_clients_input(void)
+{
 	std::string	input;
 	int			ret;
-	
+
 	for (int i = 0; i < NB_MAX_CLI; i++) {
 		ret = this->_server.get_client_input(i, input);
 		if (ret == s_recv) {
@@ -73,7 +79,8 @@ void	Matt_daemon::_log_clients_input(void) {
 	}
 }
 
-void    Matt_daemon::start(void) {
+void    Matt_daemon::start(void)
+{
 	int			valselect;
 	int			ret;
 	char		pid[30];
@@ -81,8 +88,8 @@ void    Matt_daemon::start(void) {
 	sprintf(pid, "started. PID: %d.", getpid());
 	Tintin_reporter::write_logs(Tintin_reporter::inf, "Entering Daemon mode.");
 	Tintin_reporter::write_logs(Tintin_reporter::inf, pid);
-	while(1)  
-	{  
+	while(1)
+	{
 		this->_server.reset_fds();
 		valselect = select(this->_server.get_maxfd() + 1, &this->_server.get_fds(),
 							NULL , NULL , NULL);
@@ -92,7 +99,7 @@ void    Matt_daemon::start(void) {
 										"Connection failed.", "Max clients" };
 				const std::string	log_code[] = { Tintin_reporter::log,
 										Tintin_reporter::err, Tintin_reporter::err };
-										
+
 				ret = this->_server.new_client();
 				Tintin_reporter::write_logs(log_code[ret], log_msg[ret]);
 			}
@@ -101,18 +108,22 @@ void    Matt_daemon::start(void) {
 	}
 }
 
-const char* Matt_daemon::FunctionError::what() const throw() {
+const char* Matt_daemon::FunctionError::what() const throw()
+{
 	return "Matt_daemon: Failed";
 }
 
-const char* Matt_daemon::FileLock::what() const throw() {
+const char* Matt_daemon::FileLock::what() const throw()
+{
 	return "Can't open /var/log/matt_daemon/matt_daemon.lock";
 }
 
-const char* Matt_daemon::PermissionError::what() const throw() {
+const char* Matt_daemon::PermissionError::what() const throw()
+{
 	return "Matt_daemon have to run with root rights.";
 }
 
-const char* Matt_daemon::Quit::what() const throw() {
+const char* Matt_daemon::Quit::what() const throw()
+{
 	return "Quit";
 }
--- Server.cpp
+++ Server.cpp
@@ -6,27 +6,30 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 18:09:09 by adbenoit          #+#    #+#             */
-/*   Updated: 2023/02/01 16:12:26 by adbenoit         ###   ########.fr       */
+/*   Updated: 2023/02/02 17:13:14 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server() : _cli_sd(NB_MAX_CLI, 0) {
-	this->_addr.sin_family = AF_INET;  
-	this->_addr.sin_addr.s_addr = INADDR_ANY;  
+Server::Server() : _cli_sd(NB_MAX_CLI, 0)
+{
+	this->_addr.sin_family = AF_INET;
+	this->_addr.sin_addr.s_addr = INADDR_ANY;
 	this->_addr.sin_port = htons(PORT);
 	this->_addr_len = sizeof(_addr);
 	FD_ZERO(&this->_fds);
 }
 
-Server::~Server(){
+Server::~Server()
+{
 	for (unsigned int i = 0; i < this->_cli_sd.size(); i++){
 		close(this->_cli_sd[i]);
 	}
 }
 
-int Server::get_nb_cli(void) const {
+int Server::get_nb_cli(void) const
+{
 	int	nb = 0;
 	for (unsigned int i = 0; i < this->_cli_sd.size(); i++){
 		if (this->_cli_sd[i] != 0)
@@ -36,7 +39,8 @@ int Server::get_nb_cli(void) const {
 }
 
 
-int Server::is_newclient(void) const {
+int Server::is_newclient(void) const
+{
 	return FD_ISSET(this->_m_socket, &this->_fds);
 }
 
@@ -66,9 +70,10 @@ int Server::new_client(void)
 	return (ret);
 }
 
-void Server::reset_fds(void) {
+void Server::reset_fds(void)
+{
 	FD_ZERO(&this->_fds);
-	FD_SET(this->_m_socket, &this->_fds);  
+	FD_SET(this->_m_socket, &this->_fds);
 	for (unsigned int i = 0; i < this->_cli_sd.size(); i++) {
 		if(this->_cli_sd[i] > 0) {
 			FD_SET(this->_cli_sd[i], &this->_fds);
@@ -76,9 +81,10 @@ void Server::reset_fds(void) {
 	}
 }
 
-int Server::get_maxfd(void) const {
+int Server::get_maxfd(void) const
+{
 	int	maxfd = this->_m_socket;
-	
+
 	for (size_t i = 0; i < this->_cli_sd.size(); i++) {
 		if (this->_cli_sd[i] > maxfd) {
 			maxfd = this->_cli_sd[i];
@@ -87,11 +93,13 @@ int Server::get_maxfd(void) const {
 	return (maxfd);
 }
 
-fd_set& Server::get_fds(void){
+fd_set& Server::get_fds(void)
+{
 	return this->_fds;
 }
 
-int Server::get_client_input(int i, std::string& input) {
+int Server::get_client_input(int i, std::string& input)
+{
 	size_t	valread, size;
 	char	buffer[BUFFER_SIZE + 1];
 	int		ret = -1;
@@ -102,20 +110,20 @@ int Server::get_client_input(int i, std::string& input) {
 		valread = read(this->_cli_sd[i], buffer, BUFFER_SIZE);
 		size = valread;
 		while (valread == BUFFER_SIZE) {
-			buffer[valread] = '\0';  
+			buffer[valread] = '\0';
 			input += buffer;
 			valread = read(this->_cli_sd[i], buffer, BUFFER_SIZE);
 			size += valread;
 		}
-		if (size  == 0)  
-		{  
-			close(this->_cli_sd[i]);  
+		if (size  == 0)
+		{
+			close(this->_cli_sd[i]);
 			this->_cli_sd[i] = 0;
 			ret = s_disconnect;
-		}                       
-		else 
-		{  
-			buffer[valread] = '\0';  
+		}
+		else
+		{
+			buffer[valread] = '\0';
 			input += buffer;
 			input = input.substr(0, input.find("\r")); // for telnet (ctrl + ] + q to exit)
 			input = input.substr(0, input.find("\n")); // for netcat (ctrl + c to exit)
@@ -124,9 +132,10 @@ int Server::get_client_input(int i, std::string& input) {
 	return (ret);
 }
 
-int    Server::init(void) {
+int    Server::init(void)
+{
 	int	ret = s_ok;
-	
+
 	this->_m_socket = socket(AF_INET , SOCK_STREAM , 0);
 	if(this->_m_socket  == 0) {
 		ret = s_ko;
